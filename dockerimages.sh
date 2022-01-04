d04a3563d5067512687234caa0ca874fc0dade0d
Juan David Sierra <juan.sierra@zabud.com.co>
2022-01-03 19:08:25 -0500

    verificar si la carperta github esta, sino crearla

--- dockerimages.sh
+++ dockerimages.sh
@@ -1,6 +1,10 @@
 GITHUB_HOME=$HOME/github
 ZABUD_HOME=$HOME/Zabud
 
+if [ -d $GITHUB_HOME ]; the
+    mkdir $GITHUB_HOME
+fi
+
 sudo docker run --rm -d -p 27017:27017 --name mongo-inscription mongo:5.0.3-focal
 sudo docker run --rm -d -p 5432:5432 --name pg-inscription -v $ZABUD_HOME/data/pg-inscription:/var/lib/postgresql/data -e POSTGRES_USER=postgre -e POSTGRES_DB=zabud-inscription -e POSTGRES_PASSWORD=root postgres:12.9-alpine
 sudo docker run --rm -d -p 5433:5432 --name pg-core -v $ZABUD_HOME/data/pg-core:/var/lib/postgresql/data -e POSTGRES_USER=postgre -e POSTGRES_DB=zabud-core -e POSTGRES_PASSWORD=root postgres:12.9-alpine
