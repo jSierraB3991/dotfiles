82342657367fe0a054dc807608f2bb9bfd43d2ad
Juan David Sierra <juan.sierra@zabud.com.co>
2022-01-03 19:00:12 -0500

    descargar e instalar pip for python

---
+++ install-on-python.sh
@@ -0,0 +1,8 @@
+python -m ensurepip --upgrade
+
+#https://pip.pypa.io/en/stable/installation/
+curl https://bootstrap.pypa.io/get-pip.py --output pip.py
+python get-pip.py
+
+#Update
+python -m pip install --upgrade pip
