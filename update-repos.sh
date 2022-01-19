0e60a953f2db910225d0cf828e179793aef0a9b2
Juan David Sierra <juan.sierra@zabud.com.co>
2022-01-19 16:14:24 -0500

    no update akatsuki ever run this file

--- update-repos.sh
+++ update-repos.sh
@@ -3,18 +3,27 @@ FOLDER=$(echo "${PWD}")
 ZABUD_HOME=$HOME/Zabud
 
 branch="develop"
+set -e
+
+if [ $# -eq 1 ] && [ "$1" != "-y" ]; then
+    echo "The parameter $1 not recognize"
+    exit 1
+fi
+
 for repo in $(ls $ZABUD_HOME)
 do
     if [ -d $ZABUD_HOME/$repo ] && [ -d $ZABUD_HOME/$repo/.git ]; then
 	cd $ZABUD_HOME/$repo
         if [ "$repo" == "akatsuki-repo" ]; then
-            for branch in $(echo "dev qa local")
-            do
-		if [ ! -f $branch ]; then
-                    git checkout $branch
-                    git pull origin $branch
-                fi
-            done
+            if [ $# -eq 1 ] && [ "$1" == "-y" ]; then
+                for branch in $(echo "dev qa local")
+                do
+    		    if [ ! -f $branch ]; then
+                        git checkout $branch
+                        git pull origin $branch
+                    fi
+                done
+            fi
         else
             branch=$(git branch --show-current)
             git pull origin $branch
