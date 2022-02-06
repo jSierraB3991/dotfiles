5297c676182bc326f89692094e50164a6cc930ec
Okabe Rintarou <eliotandelon@gmail.com>
2022-02-06 15:40:17 -0500

    change qtile design

--- qtile/settings/screens.py
+++ qtile/settings/screens.py
@@ -14,7 +14,7 @@ import subprocess
 def status_bar(widgets):
     return bar.Bar(widgets, 24, opacity=0.7)
 
-screens = "" #[Screen(top=status_bar(primary_widgets))]
+screens = [Screen(top=status_bar(primary_widgets))]
 
 xrandr = "xrandr | grep -w 'connected' | cut -d ' ' -f 2 | wc -l"
 
