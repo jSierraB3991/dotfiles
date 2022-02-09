6af739b2854b847549a67738cacb0c9312ced412
Juan David Sierra <Juan.Sierra@Zabud.com.co>
2022-02-09 01:17:08 -0500

    bspwm .config

--- bspwm/polybar/launch.sh
+++ bspwm/polybar/launch.sh
@@ -13,6 +13,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 ## Left bar
 polybar log -c ~/.config/polybar/current.ini &
 polybar secondary -c ~/.config/polybar/current.ini &
+polybar terceary -c ~/.config/polybar/current.ini &
+polybar cuaternary -c ~/.config/polybar/current.ini &
 
 ## Right bar
 polybar top -c ~/.config/polybar/current.ini &
