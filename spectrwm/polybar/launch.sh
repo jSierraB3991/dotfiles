7cbf8223b8c99fadb8cdf312b4928ca6ffcbb956
Juan David Sierra <Juan.Sierra@Zabud.com.co>
2022-02-12 03:21:42 -0500

    change polybar for spectrwm

--- spectrwm/polybar/launch.sh
+++ spectrwm/polybar/launch.sh
@@ -11,14 +11,12 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 ## Launch
 
 ## Left bar
-polybar log -c ~/.config/polybar/current.ini &
 polybar secondary -c ~/.config/polybar/current.ini &
 polybar terceary -c ~/.config/polybar/current.ini &
 polybar cuaternary -c ~/.config/polybar/current.ini &
 
 ## Right bar
 polybar top -c ~/.config/polybar/current.ini &
-polybar primary -c ~/.config/polybar/current.ini &
 
 ## Center bar
 polybar primary -c ~/.config/polybar/workspace.ini &
