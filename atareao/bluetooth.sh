d1b5773027d52bdee8ea3ddb9775d7a4c31eb651
Juan David Sierra <juan.sierra@zabud.com.co>
2022-03-22 15:25:54 -0500

    using theme for default in bluetooth connections

--- atareao/bluetooth.sh
+++ atareao/bluetooth.sh
@@ -10,7 +10,7 @@ bluetoothctl devices | {
         moptions["${option:1}"]="${mac}"
         question+="${option:1}\n"
     done
-    ans=$(echo -e "${question::-2}" | rofi -dmenu)
+    ans=$(echo -e "${question::-2}" | rofi -show combi -theme Monokai -icon-theme Tela-circle -dmenu)
     rs=$?
     if [ $rs -eq 0 ]
     then
