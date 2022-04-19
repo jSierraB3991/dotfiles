9e06c2d944f84ac66edc4fcecb24d07d9eb9fae7
Juan David Sierra <juan.sierra@zabud.com.co>
2022-04-19 09:45:24 -0500

    add send notify, when add screenshot

--- scripts/scrot.sh
+++ scripts/scrot.sh
@@ -5,4 +5,5 @@ format_screen=$1
 
 if [ "$format_screen" == "-s" ] || [ "$format_screen" == "-m" ]; then
     scrot $format_screen $HOME/Images/$name_image.png
+    notify-send "Scrrenshot save in $HOME/Images/$name_image.png" -u low
 fi
