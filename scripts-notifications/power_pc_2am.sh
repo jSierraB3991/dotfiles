e6238bc063957788a35beb466f641c81b0d77dbf
Juan David Sierra <juan.sierra@zabud.com.co>
2022-04-20 23:40:51 -0500

    add notify critical, for 30 seconds for shutdown pc

--- scripts-notifications/power_pc_2am.sh
+++ scripts-notifications/power_pc_2am.sh
@@ -9,7 +9,20 @@ while true; do
     fi
 
     if [ $date_hour -ge 02 ] && [ $date_hour -lt 06 ]; then
+
+        for num in $(echo "1 2 3"); do
+
+            faltan=30
+            if [ $num -eq 2 ]; then
+                faltan=20
+            elif [ $num -eq 3 ]; then
+                faltan=10
+            fi
+
+            notify-send "The Pc shutdown in $faltan" -h string:x-canonical-private-synchronous:poweroff -u critical
+            sleep 10
+        done
         systemctl poweroff
     fi
-    sleep 10
+    sleep 5
 done
