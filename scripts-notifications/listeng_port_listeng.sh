82536649b74f1ed931f8dcf4375dae24f9daf314
Juan David Sierra <juan.sierra@zabud.com.co>
2022-04-23 00:38:50 -0500

    delete break for view ports

--- scripts-notifications/listeng_port_listeng.sh
+++ scripts-notifications/listeng_port_listeng.sh
@@ -9,12 +9,10 @@ while true; do
     if [ "$num_ports" != "$num_ports2" ]; then
 
         port_change=23
-        method="up"
         ports_new=$(ss -tulpn | grep LISTEN | awk '{print $5}' | grep "^[1 | 0 | * ]" | awk 'BEGIN{FS=":"} {print $2}')
         is_add=1
         if [ $num_ports -lt $num_ports2 ]; then
             #up a service in a port
-            method="up"
             for port_new in ${ports_new[*]}; do
                 for port in ${ports[*]}; do
                     if [ $port_new -eq $port ]; then
@@ -24,14 +22,13 @@ while true; do
                 done
                 if [ $is_add -eq 1 ]; then
                     port_change=$port_new
-                    break
+                    notify-send "A port $port_change is change to up"
                 else
                     is_add=1
                 fi
             done
         else
             #down a service in a port
-            method="down"
             for port in ${ports[*]}; do
                 for port_new in ${ports_new[*]}; do
                     if [ $port_new -eq $port ]; then
@@ -43,14 +40,12 @@ while true; do
                     is_add=1
                 else
                     port_change=$port
+                    notify-send "A port $port_change is change to down"
                 fi
             done
         fi
-
         num_ports=$num_ports2
         ports=$ports_new
-        notify-send "A port $port_change is change to $method"
-        echo "ports listeng is: $num_ports"
     fi
     sleep 2
 done
