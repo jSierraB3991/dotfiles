d3c87e7ff2088186a5e1e36f1d98602bece322a5
Juan David Sierra <juan.sierra@zabud.com.co>
2022-05-03 00:51:22 -0500

    add random background to config

--- scripts-notifications/run-notification.sh
+++ scripts-notifications/run-notification.sh
@@ -4,9 +4,13 @@ killall notas.sh
 killall verify_battery.sh
 killall power_pc_2am.sh
 killall listeng_port_listeng.sh
+killall change_automatic_wallpaper.sh
+
 clipcatctl clear
 
+~/Source/scripts/bash/change_automatic_wallpaper.sh &
 ~/scripts/notas.sh &
 ~/scripts/verify_battery.sh &
 ~/Source/scripts/bash/power_pc_2am.sh &
 ~/Source/scripts/bash/listeng_port_listeng.sh &
+
