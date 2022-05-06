90eefe6d58288195128e4014760f6d3b1f1100d7
Juan David Sierra <juan.sierra@zabud.com.co>
2022-05-06 09:52:00 -0500

    add clipcat in start tilings manager

--- scripts-notifications/run-notification.sh
+++ scripts-notifications/run-notification.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
+killall clipcatd
 killall notas.sh
 killall verify_battery.sh
 killall power_pc_2am.sh
 killall listeng_port_listeng.sh
 killall change_automatic_wallpaper.sh
 
+clipcatd
 clipcatctl clear
 
 ~/Source/scripts/bash/change_automatic_wallpaper.sh &
