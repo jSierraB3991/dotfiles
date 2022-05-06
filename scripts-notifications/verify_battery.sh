8fdf2f72e3cd939a79bc06af616736db05b16477
Juan David Sierra <juan.sierra@zabud.com.co>
2022-05-06 09:50:44 -0500

    add image to charge in notify

--- scripts-notifications/verify_battery.sh
+++ scripts-notifications/verify_battery.sh
@@ -6,12 +6,16 @@ while true; do
     if [ $batstat == 'Unknown' ]; then
         notify-send "This computer not contains battery, finalizaing script" -u low
         killall verify_battery.sh
-    elif [[ $battery -ge 5 ]] && [[ $battery -le 20 ]]; then
+    elif [[ $battery -ge 1 ]] && [[ $battery -le 30 ]]; then
         notify_id=508
+        batstat="$(/usr/bin/cat /sys/class/power_supply/BAT1/status)"
         icon="/usr/share/icons/gnome/48x48/status/battery-caution.png"
+        if [ "$batstat" == "Charging" ]; then
+            icon="/usr/share/icons/gnome/48x48/status/battery-low-charging.png"
+        fi
         number=$(cat /sys/class/power_supply/BAT1/capacity)
 
         dunstify -u critical -i $icon -r $notify_id "Battery low percentaje: $number%"
     fi
-    sleep 9
+    sleep 1
 done
