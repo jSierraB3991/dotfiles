52cfac916a665990e0431b8c63b81847261d8ea9
Juan David Sierra <judas3991@gmail.com>
2022-05-29 21:01:00 -0500

    reconfigutaing of scripts

--- i3/scripts/auto-start.sh
+++ i3/scripts/auto-start.sh
@@ -1,18 +1,11 @@
 #! /bin/bash
 
-killall clipcatd
 killall listeng_port_listeng.sh
 killall change_automatic_wallpaper.sh
 killall deadd-notification-center
 killall verify_battery.sh
-killall notas.sh
-killall power_pc_2am.sh
-
-clipcatd
-clipcatctl clear
 
 ~/Source/scripts/bash/change_automatic_wallpaper.sh &
 ~/Source/scripts/bash/listeng_port_listeng.sh &
 ~/Source/scripts/bash/verify_battery.sh &
 deadd-notification-center &
-~/Source/scripts/bash/power_pc_2am.sh &
