efcf2930310deedf91d46ecd7fb6e0b896b57085
Juan David Sierra <juan.sierra@zabud.com.co>
2022-07-12 19:23:13 -0500

    remove bar of rigth

--- bspwm/polybar/launch.sh
+++ bspwm/polybar/launch.sh
@@ -14,9 +14,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar log -c ~/.config/polybar/current.ini &
 polybar secondary -c ~/.config/polybar/current.ini &
 
-## Right bar
-#polybar top -c ~/.config/polybar/current.ini &
-polybar primary -c ~/.config/polybar/current.ini &
-
 ## Center bar
 polybar primary -c ~/.config/polybar/workspace.ini &
