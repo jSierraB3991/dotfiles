0e645862d81ba1f20e42ba119d25b51473e88498
Juan David Sierra <juan.sierra@zabud.com.co>
2022-07-21 09:51:42 -0500

    configurate bspwm and polybar for two monitors

--- bspwm/polybar/launch.sh
+++ bspwm/polybar/launch.sh
@@ -15,4 +15,12 @@ polybar log -c ~/.config/polybar/current.ini &
 polybar secondary -c ~/.config/polybar/current.ini &
 
 ## Center bar
-polybar primary -c ~/.config/polybar/workspace.ini &
+my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-A-0' | awk '{print $2}' )
+echo $my_laptop_external_monitor
+if [ $my_laptop_external_monitor = "connected" ]
+then
+    polybar primary -c ~/.config/polybar/workspace.ini &
+    polybar primary_external -c ~/.config/polybar/workspace.ini &
+else
+    polybar primary_one -c ~/.config/polybar/workspace.ini &
+fi
