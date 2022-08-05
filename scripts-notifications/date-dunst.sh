47e8077e456c539c900c27572eed601352fd616e
Juan David Sierra <judas3991@gmail.com>
2022-08-05 17:29:16 -0500

    remove generics scripts

--- scripts-notifications/date-dunst.sh
+++ scripts-notifications/date-dunst.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+id=0
 while true; do
     date=$(date)
-    notify-send "$date" -h string:x-canonical-private-synchronous:my-notification -u low
+
+    new_id=$(notify-send "$date" -h string:x-canonical-private-synchronous:my-notification -u low -p --replace-id=$id -a CALENDAR -i date)
+    id=$new_id
     sleep 1
 done
