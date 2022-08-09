e0dc65f1a2f37f693af4de09630ff3f3d6a1f79b
Juan David Sierra <judas3991@gmail.com>
2022-08-09 08:57:22 -0500

    onfigure autostart in i3

--- i3/scripts/auto-start.sh
+++ i3/scripts/auto-start.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
 killall listeng_port_listeng.sh
-killall change_automatic_wallpaper.sh
 killall deadd-notification-center
 killall verify_battery.sh
 
-~/Source/scripts/bash/change_automatic_wallpaper.sh &
 ~/Source/scripts/bash/listeng_port_listeng.sh &
 ~/Source/scripts/bash/verify_battery.sh &
 deadd-notification-center &
