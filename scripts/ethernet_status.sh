d0efe92413778e60705b97a50c35010c0fc0f71e
Juan David Sierra <judas3991@gmail.com>
2022-08-23 01:34:38 -0500

    remove all unnecesary scripts

--- scripts/ethernet_status.sh
+++
@@ -1,9 +0,0 @@
-
-    file="/home/juan-sierra/.config/bin/data.txt"
-    curl -s checkip.dyndns.org > $file 
-
-    if [ "$(cat $file | wc -l)" = "1" ]; then
-        echo "$(cat $file | grep -Eo '[0-9.]+')"
-    else
-        echo "BAD GATEWAY"
-    fi
