92611fbe1c0b70825317a4dee0828c0b954ecee9
Juan David Sierra <judas3991@gmail.com>
2022-09-02 08:18:12 -0500

    updat script for brightness for show notification

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 
+function show_notification() {
+    message=$(brightnessctl | grep "Current" | awk '{print $4}')
+    replace_id=5
+    id=$(notify-send "Brightness show is $message" -u low -i gpm-brightness-lcd --replace-id $replace_id -p)
+}
+
 case $1 in
-    up)
-        brightnessctl set 5%+
-        ;;
-    down)
-        brightnessctl set 5%-
-	    ;;
-    *)
-        echo "Usage: $0 up | down "
-        ;;
+    "up") brightnessctl set 5%+ 
+        show_notification;;
+    "down") brightnessctl set 5%- 
+        show_notification;;
+    *) echo "Usage: $0 up | down ";;
 esac
