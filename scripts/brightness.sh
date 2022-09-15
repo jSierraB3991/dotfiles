4b46e837afbcc58c1093bb183650ef2307eb83e1
Juan David Sierra <judas3991@gmail.com>
2022-09-15 11:05:21 -0500

    change terminal to xfce4-terminal, and remove memory to is bar status

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/bash
 
 function show_notification() {
+    replace_id=0
+    if [ ! -f /home/juan-sierra/.local/data/brightnessctl_id.txt ]; then
+        echo "creating file"
+        touch /home/juan-sierra/.local/data/brightnessctl_id.txt
+    else
+        replace_id=$(cat /home/juan-sierra/.local/data/brightnessctl_id.txt)
+    fi
+    echo "Hola $replace_id"
     message=$(brightnessctl | grep "Current" | awk '{print $4}')
-    replace_id=5
-    id=$(notify-send "Brightness show is $message" -u low -i gpm-brightness-lcd --replace-id $replace_id -p)
+    if [ "$replace_id" == "" ]; then
+        replace_id=0
+    fi
+    id=$(notify-send "Brightness show is $message" -u low -i brightness-systray --replace-id $replace_id -p)
+    echo $id > /home/juan-sierra/.local/data/brightnessctl_id.txt
 }
 
 case $1 in
