496f7b81ecbed1a935e694c6539049bf087c64aa
Juan David Sierra <juan.sierra@zabud.com.co>
2022-11-03 22:22:46 -0500

    reconfigurate startup apps init ot spectrwm

--- spectrwm/run-startup.sh
+++ spectrwm/run-startup.sh
@@ -1,16 +1,27 @@
 #! /bin/bash
 
+killall picom
+killall copyq
+killall dunst
+killall blueman-applet
+killall xscreensaver
+killall listeng_port_listeng_diff.sh
+killall verify_battery.sh
+
 /home/juan-sierra/Source/scripts/bash/change_automatic_wallpaper_one.sh &
 picom &
 copyq &
+dunst &
 blueman-applet &
-deadd-notification-center &
+xscreensaver -no-splash &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 eval "$(dbus-launch)"
+export $(dbus-launch)
+
 
 /home/juan-sierra/Source/scripts/bash/listeng_port_listeng_diff.sh &
 /home/juan-sierra/Source/scripts/bash/verify_battery.sh &
-yakuake &
+#yakuake &
 
 disown
 disown -a
