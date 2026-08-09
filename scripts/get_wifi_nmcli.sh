19054fb7e974e65c20bfeb7e55601102e4304372
Juan David Sierra <judas3991@gmail.com>
2026-08-08 19:28:55 -0500

    add configuration if etehernet if conected

--- scripts/get_wifi_nmcli.sh
+++ scripts/get_wifi_nmcli.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
+device_conect=$(nmcli dev | grep -w "conectado" | awk '{print $2}')
+if [[ "$device_conect" == "ethernet" ]]; then
+    echo "󰈀 Interface cableada"
+else
+    nmcli dev wifi list | grep '*' | awk '{$1="";$2=""; print $0}' | awk 'BEGIN{FS="Infra"}{print $1}'
+fi 
 
-nmcli dev wifi list | grep '*' | awk '{$1="";$2=""; print $0}' | awk 'BEGIN{FS="Infra"}{print $1}'
+
